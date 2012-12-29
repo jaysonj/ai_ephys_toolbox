7ce5d60e6f895b82951a53c7397efcabd2f7760c
Jayson Jochim <jayson.jochim@gmail.com>
2012-12-29 12:06:19 -0800

    Intitial Commit

---
+++ intro.m
@@ -0,0 +1,12 @@
+%% Collection of MATLAB files for Electrophysiology
+%  Allen Institute
+%  Jay Jochim, 12.12.2012
+%  
+%  Organization: 
+%  ex: package includes scripts used for data collection
+%  da: package includes scripts used for data analysis
+%  demo: includes sample data 
+%  
+%  Dependancies: 
+%  FMAtoolbox <http://fmatoolbox.sourceforge.net/> (Michaël Zugaro)
+
